0360b60f34c2246f8d47fc2e1bdb5033e00e41aa
Yanda Bao <yandabaodav@gmail.com>
2022-10-07 20:56:30 -0700

    1.9.2 edits to JSON, BloodSugarGraph

--- components/BloodSugarGraph.js
+++ components/BloodSugarGraph.js
@@ -1,15 +1,55 @@
-import { Text, SafeAreaView } from 'react-native';
+import { Text, SafeAreaView, Dimensions, StyleSheet } from 'react-native';
 import { general, bloodSugarGraph } from './../style/style';
+import { LineChart, BarChart, PieChart, ProgressChart, ContributionGraph, StackedBarChart} from "react-native-chart-kit";
 
 export default function BloodSugarGraph() {
   const sugarData = [95, 99, 112, 100, 130, 140];
   let yHideRatio = 0.8;
   let defaultMax = 250;
 
+  let opacity = 0.5;
   return (
-    <SafeAreaView>
-      <Text>
-        {sugarData[1]}
-      </Text>
-    </SafeAreaView>);
+    <LineChart
+    data={{
+      labels: ["January", "February", "March", "April", "May", "June"],
+      datasets: [
+        {
+          data: [
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100
+          ]
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={220}
+    yAxisLabel="$"
+    yAxisSuffix="k"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${0.5})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${0.5})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />);
 }
